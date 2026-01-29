2cb00b3aaa368a0327fa6c8d71ddd57d2b4dff4f
JU30N <joongwon.hong@gmail.com>
2026-01-29 13:03:14 +0100

    lab3 done and on lab 4

--- Laboration4/bfs.py
+++ Laboration4/bfs.py
@@ -4,7 +4,8 @@ from linkedQFile import LinkedQ
 svenska = Bintree()#svenska tre bokstäver ord 
 gamla = Bintree()#besökta ställen 
 
-with open("C:/Users/joong/Desktop/DD1320/DD1320_applied_CS/Laboration4/word3.txt", "r", encoding="utf-8") as svenskfil:
+#with open("C:/Users/joong/Desktop/DD1320/DD1320_applied_CS/Laboration4/word3.txt", "r", encoding="utf-8") as svenskfil:
+with open("/Users/ju30n/DD1320_applied_CS/Laboration4/word3.txt", "r", encoding="utf-8") as svenskfil:
     for rad in svenskfil:
         ordet = rad.strip()                # Ett trebokstavsord per rad
         #print(ordet)
@@ -18,6 +19,18 @@ with open("C:/Users/joong/Desktop/DD1320/DD1320_applied_CS/Laboration4/word3.txt
 #i startordet (aöt, böt, ..., söö), kolla att det nya ordet finns i
 #ordlistan men inte finns i gamla och i så fall skriva ut det nya ordet på
 #skärmen och lägga in det i gamla.
+
+#byta ord -> kolla om den finns i svenska och inte besökt -> hittat?
+#                                                           -> lägg till ordet i kön för att leta vidare från det ordet vi har hittat
+# ex leta från 1 till 6
+#.      1
+#   2           3       5   6
+#       4
+#börja 1 -> gå till 2 och lägg till 2 i kön och 3 -> från 2 hittat 4 lägg till i kön -> nästa i kön 3 hittat 4 5 lägg till i kö men 4 redan i besökt så lägg inte till i kön
+#besök nästa i kön 4 -> inget -> 5 hittar 6 lägg till i kön -> hittat 6!
+#
+#kön[1] -> [2,3] -> [3] -> [3,4]-> [4,5]-> [5]-> [6]
+#besökt[1]-> [1,2]->[1,2,3]-> [1,2,3,4]->[1,2,3,4,5]
 
 def makechildren(start_word, end_word, q):
     
@@ -37,7 +50,7 @@ def makechildren(start_word, end_word, q):
 
             if (new_word in svenska) and (new_word not in gamla):#kolla om den finns här eller inte
                 #print(new_word)
-                gamla.put(new_word)
+                gamla.put(new_word)#lägg till att vi har besökt detta ord 
 
                 if new_word == end_word:#hittat
                     return True
@@ -52,9 +65,9 @@ def makechildren(start_word, end_word, q):
 def main():
     
     #start_word = input("Start ord: ")
-    starting_word = "ska"
+    starting_word = "luv"
     #ending_word = input("Slut ord: ")
-    end_wording = "luv"
+    end_wording = "soa"
     q = LinkedQ()
     q.enqueue(starting_word)
     gamla.put(starting_word)
@@ -62,7 +75,7 @@ def main():
     while not q.isEmpty():
         word = q.dequeue()#ful
         foundword = makechildren(word, end_wording, q) #ställen den har varit på och första som finns i listan
-        if foundword:#om detta är true printa lösningen
+        if foundword:#kolla om hittat ordet
             break
 
     if foundword == None:
---
+++ Laboration5/upgrade_lab4.py
@@ -0,0 +1,10 @@
+def utskrift(lista):
+    if len(lista) > 0:
+        print(lista[0])
+        utskrift(lista[1:])
+
+def main():
+    lista = [1,2,3,4,5]
+    utskrift(lista)
+
+main()
